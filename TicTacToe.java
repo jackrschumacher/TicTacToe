46cc585dac287f4b054e8b06d34d1bb79909c52d
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-28 17:26:06 +0000

    Initial setup and comments

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,8 @@
 public class TicTacToe{
   private char [][] board;
+  // When turn is 0, the X player is playing
+  // When turn is 1, the 0 player is playing
+  private int turn;
 
 
 
@@ -11,7 +14,16 @@ public class TicTacToe{
       {' ', ' ', ' '},
       {' ', ' ', ' '},
       {' ', ' ', ' '},
-    }
+    };
+  }
+  /*Description: Place an X or 0 if the grid is still empty at that location
+  * Precondition: 2D array has been intialiszed, player submits a row number from 0 to 2 and a column number from 0 to 2
+  * Postcondition: true is reutrned if the space was avaliable, and the board is updated
+  * False is reutnred if the space was not avalialbe, and the board is unchanged. 
+  * If a peice is placed, swap turns
+  */
+
+  private boolean PlacePeice(int row, int col){
     
   }
 }
