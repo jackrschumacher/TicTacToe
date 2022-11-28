b289af1429d6a59305b58aeb399ef6b788ff3350
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-28 17:17:24 +0000

    Initial commit

---
+++ TicTacToe.java
@@ -0,0 +1,17 @@
+public class TicTacToe{
+  private char [][] board;
+
+
+
+// Description: initializes the board variable to have empty spaces using the space charachter in a 3*3 grid
+
+  private void SetUpBoard(){
+    // declare the arrays
+    this.board = new char[][]{
+      {' ', ' ', ' '},
+      {' ', ' ', ' '},
+      {' ', ' ', ' '},
+    }
+    
+  }
+}
